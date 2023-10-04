082645cb76046d770a721f2d1bf0e838b0b6bc0c
ChaesongYun <139418987+ChaesongYun@users.noreply.github.com>
2023-10-04 22:13:51 +0900

    Update SUB_QUERY.sql

--- GROUP_BY/SUB_QUERY.sql
+++ GROUP_BY/SUB_QUERY.sql
@@ -22,3 +22,15 @@ ORDER BY MONTH(START_DATE), CAR_ID DESC
 CAR_ID를 그룹지었을 때 5개 이상인 ID를 테이블로 만들어서 거기에 해당하는 ID만 출력해라
 GROUP BY MONTH, CAR_ID 하면 어짜피 중복 제거되니 COUNT(*) 하면 해당 월, 해당 아이디에 맞는 개수가 구해짐
 */
+
+
+/*
+FOOD_PRODUCT 테이블에서 식품분류별로 가격이 제일 비싼 식품의 분류, 가격, 이름을 조회하는 SQL문을 작성해주세요. 
+이때 식품분류가 '과자', '국', '김치', '식용유'인 경우만 출력시켜 주시고 
+결과는 식품 가격을 기준으로 내림차순 정렬해주세요.
+*/
+    
+SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
+FROM FOOD_PRODUCT
+WHERE PRICE IN (SELECT MAX(PRICE) FROM FOOD_PRODUCT GROUP BY CATEGORY) AND CATEGORY IN ('과자', '국', '김치', '식용유')
+ORDER BY MAX_PRICE DESC
